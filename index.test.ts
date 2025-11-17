4e863d96d68596600e1008f65a6c008aead6f4c0
takumab <takumab@gmail.com>
2025-11-17 08:33:45 +0000

    Add prettier

--- index.test.ts
+++ index.test.ts
@@ -27,26 +27,29 @@ type Customer = {
 };
 
 type Account = {
-	id: string;
+  id: string;
   balance: number;
   customer: Customer;
 };
 
 class AccountEventHandler {
-	private readonly eventsRepository: EventsRepository;
+  private readonly eventsRepository: EventsRepository;
   private readonly accountEventProcessor: AccountEventProcessor;
 
-  constructor({eventsRepository, accountEventProcessor}: {
-	  eventsRepository: EventsRepository,
-	  accountEventProcessor: AccountEventProcessor
+  constructor({
+    eventsRepository,
+    accountEventProcessor,
+  }: {
+    eventsRepository: EventsRepository;
+    accountEventProcessor: AccountEventProcessor;
   }) {
-	  this.eventsRepository = eventsRepository;
+    this.eventsRepository = eventsRepository;
     this.accountEventProcessor = accountEventProcessor;
   }
 
   async send(event: AccountCreatedEvent) {
-		await this.eventsRepository.save(event);
-	  await this.accountEventProcessor.buildProjection(event.payload.accountId)
+    await this.eventsRepository.save(event);
+    await this.accountEventProcessor.buildProjection(event.payload.accountId);
   }
 }
 
@@ -54,67 +57,89 @@ class AccountsRepository {
   private readonly accounts: Array<Account> = [];
 
   async findAllBy(email: string): Promise<Array<Account>> {
-    return this.accounts.filter((account: Account) => account.customer.email === email);
+    return this.accounts.filter(
+      (account: Account) => account.customer.email === email,
+    );
   }
 
   async save(account: Account) {
     this.accounts.push(account);
   }
 
-	async findById(accountId: string): Promise<Account | undefined> {
-		return this.accounts.find((account: Account) => account.id === accountId)
-	}
+  async findById(accountId: string): Promise<Account | undefined> {
+    return this.accounts.find((account: Account) => account.id === accountId);
+  }
 }
 
 class EventsRepository {
-	private events: BaseAccountEvent[] = [];
-	async findById(eventId: string): Promise<BaseAccountEvent | undefined> {
-		return this.events.find((event: BaseAccountEvent) => event.eventId === eventId)
-	}
+  private events: BaseAccountEvent[] = [];
+  async findById(eventId: string): Promise<BaseAccountEvent | undefined> {
+    return this.events.find(
+      (event: BaseAccountEvent) => event.eventId === eventId,
+    );
+  }
 
-	async save(event: BaseAccountEvent) {
-		this.events.push(event)
-	}
+  async save(event: BaseAccountEvent) {
+    this.events.push(event);
+  }
 
-	findAllBy(accountId: string) {
-		return this.events.filter((event: BaseAccountEvent) => event.payload.accountId === accountId)
-	}
+  findAllBy(accountId: string) {
+    return this.events.filter(
+      (event: BaseAccountEvent) => event.payload.accountId === accountId,
+    );
+  }
 }
 
 class AccountEventProcessor {
-	private eventsRepository: EventsRepository;
-	private accountsRepository: AccountsRepository;
-	constructor({ eventsRepository, accountsRepository }: {eventsRepository: EventsRepository, accountsRepository: AccountsRepository}) {
-		this.eventsRepository = eventsRepository;
-		this.accountsRepository = accountsRepository;
-	}
+  private eventsRepository: EventsRepository;
+  private accountsRepository: AccountsRepository;
+  constructor({
+    eventsRepository,
+    accountsRepository,
+  }: {
+    eventsRepository: EventsRepository;
+    accountsRepository: AccountsRepository;
+  }) {
+    this.eventsRepository = eventsRepository;
+    this.accountsRepository = accountsRepository;
+  }
 
-	async buildProjection(accountId: string) {
-		// TODO: support other event types beyond accountCreatedEvents
-		const createdAccountEvents = this.eventsRepository.findAllBy(accountId).filter(isAccountCreatedEvent)
-		const emptyAccount = { id: "", balance: 0, customer: { email: "" } };
+  async buildProjection(accountId: string) {
+    // TODO: support other event types beyond accountCreatedEvents
+    const createdAccountEvents = this.eventsRepository
+      .findAllBy(accountId)
+      .filter(isAccountCreatedEvent);
+    const emptyAccount = { id: "", balance: 0, customer: { email: "" } };
 
-		const account = createdAccountEvents.reduce((account, event) => {
-				return {
-					...account,
-					id: accountId,
-					balance: 0,
-					customer: { email: event.payload.email }
-				}
-		}, emptyAccount);
+    const account = createdAccountEvents.reduce((account, event) => {
+      return {
+        ...account,
+        id: accountId,
+        balance: 0,
+        customer: { email: event.payload.email },
+      };
+    }, emptyAccount);
 
-		await this.accountsRepository.save(account);
-	}
+    await this.accountsRepository.save(account);
+  }
 }
 
-const isAccountCreatedEvent = (event: BaseAccountEvent): event is AccountCreatedEvent => {
-	return event.eventType === EventType.AccountCreated;
-}
+const isAccountCreatedEvent = (
+  event: BaseAccountEvent,
+): event is AccountCreatedEvent => {
+  return event.eventType === EventType.AccountCreated;
+};
 
 const eventsRepository = new EventsRepository();
 const accountsRepository = new AccountsRepository();
-const accountEventProcessor = new AccountEventProcessor({eventsRepository, accountsRepository});
-const accountEventHandler = new AccountEventHandler({eventsRepository, accountEventProcessor});
+const accountEventProcessor = new AccountEventProcessor({
+  eventsRepository,
+  accountsRepository,
+});
+const accountEventHandler = new AccountEventHandler({
+  eventsRepository,
+  accountEventProcessor,
+});
 
 describe("Bank Account", () => {
   describe("when new AccountCreatedEvent is received", () => {
@@ -141,8 +166,6 @@ describe("Bank Account", () => {
   });
 });
 
-
-
 describe("AccountEventHandler", () => {
   test("should save the event in events repository", async () => {
     const email = "takuma@example.com";
@@ -158,12 +181,17 @@ describe("AccountEventHandler", () => {
       payload,
     };
     const eventsRepository = new EventsRepository();
-    const accountEventProcessor = new AccountEventProcessor({eventsRepository, accountsRepository});
-    const accountEventHandler = new AccountEventHandler(
-      {eventsRepository: eventsRepository, accountEventProcessor: accountEventProcessor},
-    );
 
-		await accountEventHandler.send(accountCreatedEvent);
+    const accountEventProcessor = new AccountEventProcessor({
+      eventsRepository,
+      accountsRepository,
+    });
+    const accountEventHandler = new AccountEventHandler({
+      eventsRepository: eventsRepository,
+      accountEventProcessor: accountEventProcessor,
+    });
+
+    await accountEventHandler.send(accountCreatedEvent);
 
     const result = await eventsRepository.findById(accountCreatedEvent.eventId);
     expect(result).toEqual(accountCreatedEvent);
@@ -171,31 +199,34 @@ describe("AccountEventHandler", () => {
 });
 
 describe("AccountEventProcessor", () => {
-	test("should build an account projection", async () => {
-		const email = "takuma@example.com";
-		const accountId = "123";
-		const payload: AccountCreatedEventPayload = {
-			accountId,
-			email,
-		};
+  test("should build an account projection", async () => {
+    const email = "takuma@example.com";
+    const accountId = "123";
+    const payload: AccountCreatedEventPayload = {
+      accountId,
+      email,
+    };
 
-		const accountCreatedEvent: AccountCreatedEvent = {
-			eventId: "1",
-			eventType: EventType.AccountCreated,
-			payload,
-		};
+    const accountCreatedEvent: AccountCreatedEvent = {
+      eventId: "1",
+      eventType: EventType.AccountCreated,
+      payload,
+    };
 
-		const eventsRepository = new EventsRepository();
-		await eventsRepository.save(accountCreatedEvent);
+    const eventsRepository = new EventsRepository();
+    await eventsRepository.save(accountCreatedEvent);
 
-	 	const accountEventProcessor = new AccountEventProcessor({eventsRepository, accountsRepository});
-	  await accountEventProcessor.buildProjection(accountId);
+    const accountEventProcessor = new AccountEventProcessor({
+      eventsRepository,
+      accountsRepository,
+    });
+    await accountEventProcessor.buildProjection(accountId);
 
-		const account = await accountsRepository.findById(accountId);
-		expect(account).toEqual({
-			id: accountId,
-			balance: 0,
-			customer: { email }
-		});
-	});
+    const account = await accountsRepository.findById(accountId);
+    expect(account).toEqual({
+      id: accountId,
+      balance: 0,
+      customer: { email },
+    });
+  });
 });
